4d2db632ae048633dd015238de4da8d502acb596
johnmanjiro13 <juntama193@gmail.com>
2022-04-09 23:56:37 +0900

    Implement a recursive descendent parser.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
-use std::{env, process};
+use std::{env, process, thread::panicking};
 
+const REGS: [&str; 8] = ["rdi", "rsi", "r10", "r11", "r12", "r13", "r14", "r15"];
+static mut cur: usize = 0;
+
+// Tokenizer
 enum TokenType {
     Num,
 }
@@ -50,9 +54,103 @@ fn tokenize(mut p: String) -> Vec<Token> {
     tokens
 }
 
-fn fail(token: &Token) {
-    eprintln!("unexpected char: {:?}", token);
-    process::exit(1);
+// Recursive-descendent parser
+
+enum NodeType {
+    Num,
+}
+
+#[derive(Default)]
+struct Node {
+    ty: i32,
+    lhs: Option<Box<Node>>,
+    rhs: Option<Box<Node>>,
+    val: i32,
+}
+
+impl Node {
+    fn new(op: i32, lhs: Box<Node>, rhs: Box<Node>) -> Self {
+        Self {
+            ty: op,
+            lhs: Some(lhs),
+            rhs: Some(rhs),
+            ..Default::default()
+        }
+    }
+
+    fn new_num(val: i32) -> Self {
+        Self {
+            ty: NodeType::Num as i32,
+            val,
+            ..Default::default()
+        }
+    }
+
+    fn number(tokens: &Vec<Token>, pos: usize) -> Self {
+        if tokens[pos].ty == TokenType::Num as i32 {
+            let val = tokens[pos].val;
+            return Self::new_num(val);
+        }
+        panic!("number expected, but got {}", tokens[pos].input);
+    }
+
+    pub fn expr(tokens: Vec<Token>) -> Self {
+        let mut pos = 0;
+        let mut lhs = Self::number(&tokens, pos);
+        pos += 1;
+
+        if tokens.len() == pos {
+            return lhs;
+        }
+
+        loop {
+            if tokens.len() == pos {
+                break;
+            }
+
+            let op = tokens[pos].ty;
+            if op != '+' as i32 && op != '-' as i32 {
+                break;
+            }
+            pos += 1;
+            lhs = Self::new(op, Box::new(lhs), Box::new(Self::number(&tokens, pos)));
+            pos += 1;
+        }
+        if tokens.len() != pos {
+            panic!("stray token: {}", tokens[pos].input);
+        }
+        lhs
+    }
+
+    // Code generator
+    fn gen(self) -> String {
+        if self.ty == NodeType::Num as i32 {
+            let reg: &str;
+            unsafe {
+                if cur > REGS.len() {
+                    panic!("register exhausted");
+                }
+                reg = REGS[cur];
+                cur += 1;
+            }
+            println!("  mov {}, {}", reg, self.val);
+            return reg.into();
+        }
+
+        let dst = self.lhs.unwrap().gen();
+        let src = self.rhs.unwrap().gen();
+        match self.ty as u8 as char {
+            '+' => {
+                println!("  add {}, {}", dst, src);
+                dst
+            }
+            '-' => {
+                println!("  sub {}, {}", dst, src);
+                dst
+            }
+            _ => panic!("unknown operator"),
+        }
+    }
 }
 
 fn main() {
@@ -64,40 +162,13 @@ fn main() {
     }
 
     let tokens = tokenize(args.nth(1).unwrap());
+    let node = Node::expr(tokens);
 
     println!(".intel_syntax noprefix");
     println!(".global main");
     println!("main:");
 
-    if tokens[0].ty != TokenType::Num as i32 {
-        fail(&tokens[0]);
-    }
-    println!(" mov rax, {}", tokens[0].val);
-
-    let mut i = 1;
-    while i != tokens.len() {
-        if tokens[i].ty == '+' as i32 {
-            i += 1;
-            if tokens[i].ty != TokenType::Num as i32 {
-                fail(&tokens[i]);
-            }
-            println!("  add rax, {}", tokens[i].val);
-            i += 1;
-            continue;
-        }
-
-        if tokens[i].ty == '-' as i32 {
-            i += 1;
-            if tokens[i].ty != TokenType::Num as i32 {
-                fail(&tokens[i]);
-            }
-            println!("  sub rax, {}", tokens[i].val);
-            i += 1;
-            continue;
-        }
-
-        fail(&tokens[i]);
-    }
+    println!(" mov rax, {}", node.gen());
 
     println!("  ret");
 }
